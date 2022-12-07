b2048d58ada6851923723679870a156fa212791b
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-12-07 20:06:38 +0200

    Add logo

--- src/components/registration-form/index.tsx
+++ src/components/registration-form/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
+
 import sprite from '../../assets/icons.svg'
+import ProgressMobileStepper from './stepper'
 function RegistrationForm (): JSX.Element {
   return (
-    <section className='pt-4 pr-8 pb-10 pl-8'>
-      <svg className='w-8 h-8 inline-block'>
+    <section className='pt-4 pr-8 pb-10 pl-8 text-xs leading-4 w-screen'>
+      <a href="#" className='flex items-center gap-[2px]'>
+      <svg className='w-8 h-8'>
         <use href={sprite + '#chad-logo'}></use>
       </svg>
-      <h2 className='inline-block'>Chad</h2>
+      <h2 className=' not-italic font-bold text-2xl leading-8 text-Unlinked21'>Chad</h2>
+      </a>
+      <article>
+        <ProgressMobileStepper />
+      </article>
+
     </section>
   )
 }
